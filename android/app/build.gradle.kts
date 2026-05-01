61549c720d540f438a9cba831d95e78045837257
sigmarkarl <sigmarkarl@gmail.com>
2026-05-01 20:28:11 +0000

    Bundle id is.skutl.app, build 1.0.0+3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "is.skutla.app"
+    namespace = "is.skutl.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "is.skutla.app"
+        applicationId = "is.skutl.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
